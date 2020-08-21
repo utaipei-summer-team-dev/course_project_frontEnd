45fdc7682833dcb9e4c5af65242c4a38c018ff3b
AnnieKuoChia <a880318@gmail.com>
2020-08-21 13:03:13 +0800

    home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,93 +1,5 @@
 <template>
   <div class="home">
-<<<<<<< Updated upstream
-    <h2>這是首頁</h2>
-    <img alt="banner" src="../assets/img/banner.png">
-    <input type="text" class="search">
-    <ul class="hotkeys">
-      <li>設計實務</li>
-      <li>社區服務</li>
-      <li>實習</li>
-      <li>藝術</li>
-    </ul>
-    <h2 class="title">最高評價</h2>
-    <div class="scrolling-wrapper">
-      <div class="card">
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-            <div class="ratings">
-              <div class="empty_star">★★★★★</div>
-              <div class="full_star" style="width: 80%">★★★★★</div>
-            </div>
-          </div>
-        </div>
-        <div class="card">
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-            <div class="ratings">
-              <div class="empty_star">★★★★★</div>
-              <div class="full_star" style="width: 60%">★★★★★</div>
-            </div>
-          </div>
-        </div>
-        <div class="card">
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-            <div class="ratings">
-              <div class="empty_star">★★★★★</div>
-              <div class="full_star" style="width: 70%">★★★★★</div>
-            </div>
-          </div>
-        </div>
-    </div>
-    <h2 class="title">最新課程</h2>
-    <div class="scrolling-wrapper">
-      <div class="card">
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-          </div>
-        </div>
-        <div class="card">
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-          </div>
-        </div>
-        <div class="card">
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-          </div>
-        </div>
-    </div>
-    <h2 class="title">尚未評價</h2>
-    <div class="scrolling-wrapper">
-      <div class="card">
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-          </div>
-        </div>
-        <div class="card">
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-          </div>
-        </div>
-        <div class="card">
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-          </div>
-        </div>
-    </div>
-    <!-- <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-=======
     <!-- <h2>這是首頁</h2> -->
     <div class="banner">
       <img alt="banner" src="../assets/img/768banner.png">
@@ -156,21 +68,10 @@
         <div class="footer_bar"></div>
       </footer>
     </div>
->>>>>>> Stashed changes
   </div>
 </template>
 
 <script>
-<<<<<<< Updated upstream
-// @ is an alias to /src
-// import HelloWorld from '@/components/HelloWorld.vue';
-// export default {
-//   name: 'Home',
-//   components: {
-//     HelloWorld,
-//   },
-// };
-=======
 export default {
   // name: 'Home',
   data() {
@@ -188,5 +89,4 @@ export default {
       });
   },
 };
->>>>>>> Stashed changes
 </script>
